990a4bd2c38b81aed507d944e0d8eafb8b9cf2fb
SulthonShobrie <msshobrie@gmail.com>
2025-05-06 14:57:37 +0800

    modifikasi menu utama, dan menambah sepatah kata

--- menu.cpp
+++ menu.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 void dMenu(){
 system("cls");
-cout<<"Aplikasi Tampilan Menu"<<"\n";       
-cout<<"1. Menu Pertama"<<"\n";            
-cout<<"2. Menu Kedua"<<"\n";            
-cout<<"3. Menu Ketiga"<<"\n";           
-cout<<"4. Menu Keempat"<<"\n";            
+cout<<"Aplikasi Sorting Bubble"<<"\n";       
+cout<<"1. Memasukkan Data"<<"\n";            
+cout<<"2. Menampilkan Data"<<"\n";            
+cout<<"3. Sorting"<<"\n";           
+cout<<"4. Sepatah Kata"<<"\n";            
 cout<<"5. Exit"<<"\n";           
 cout<<"Masukan angka :";        
 
@@ -17,8 +17,14 @@ cout<<"Masukan angka :";
 
 void mPertama(string pesan){
 system("cls");
-cout<<"hallo saya menu "<<pesan;
+cout<<"Ini Dia Tampilannya "<<pesan;
 getch();
+}
+
+void SepatahKata(){
+  system("cls");
+  cout <<"Jangan Lupa Bernapas";
+  getch();
 }
 
 
@@ -43,7 +49,7 @@ do
     /* code */
     break;  
    case '4':
-    mPertama("ke- empat");
+    SepatahKata();
     /* code */
     break;  
   case '5':
